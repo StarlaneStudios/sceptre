3ad68f1c2bc61f33ceb01dfb86e1bc78976353c4
Julian Mills <julian.exodius@gmail.com>
2023-04-08 03:25:37 +0200

    change: many improvements

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,39 +1,68 @@
 #!/usr/bin/env node
 
-import { writeFileSync } from 'fs';
+import 'colors';
+
+import { readFileSync, writeFileSync } from 'fs';
 import { program } from 'commander';
-import { Indent, generateRoutes } from './generator.js';
+import { Indent, SceptreOptions, generateRoutes } from './generator.js';
+import { fileURLToPath } from 'node:url';
+
+const pkgPath = fileURLToPath(new URL('../package.json', import.meta.url));
+const pkgInfo = JSON.parse(readFileSync(pkgPath, 'utf-8'));
 
 program
 	.name('sceptre')
+	.version(pkgInfo.version)
 	.description('A CLI tool for compiling React Router page structures into generated imports')
 	.argument('<pattern>', 'The page glob pattern')
 	.argument('<output>', 'The output file to generate')
-	.option('--base <path>', 'The base path to use for the glob pattern')
 	.option('--indent <mode>', `Either 'none', 'tab' or an amount of spaces`)
+	.option('--base <path>', `The base path containing all pages`)
 	.action(handleGeneration)
 	.showHelpAfterError()
 	.parse();
 
 interface Options {
-	base?: string;
 	indent?: string;
+	base?: string;
 }
 
-function handleGeneration(pattern: string, output: string, options: Options) {
-	if (options.indent !== undefined && options.indent != 'none' && options.indent != 'tab' && isNaN(Number.parseInt(options.indent))) {
-		throw new Error('Invalid indent mode: ' + options.indent);
+function handleGeneration(pattern: string, output: string, { indent, base }: Options) {
+	const startTime = Date.now();
+	const config: SceptreOptions = {
+		pattern: pattern
+	};
+
+	console.log(`${'➜'.blue}  Running Sceptre version ${pkgInfo.version}`);
+	console.log(`${'➜'.blue}  Generating routing index ${output.bold}...`);
+
+	if (indent !== undefined) {
+		if (indent != 'none' && indent != 'tab' && isNaN(Number.parseInt(indent))) {
+			throw new Error('Invalid indent mode: ' + indent);
+		}
+
+		config.indent = indent as Indent;
+
+		console.log(`${'➜'.blue}  Configured indent: ${indent.cyan}`);
 	}
 
-	const index = generateRoutes({
-		basePath: options.base ?? process.cwd(),
-		indent: options.indent as Indent,
-		pattern: pattern,
-	});
+	if (base !== undefined) {
+		config.base = base;
+
+		console.log(`${'➜'.blue}  Configured base path: ${base.cyan}`);
+	}
+
+	const index = generateRoutes(config, output);
 
 	try {
 		writeFileSync(output, index);
-	} catch(err) {
-		throw new Error('Failed to generate routing index', { cause: err })
+
+		const duration = (Date.now() - startTime).toString() + 'ms';
+
+		console.log(`${'➜'.green}  Routing index generated successfully (${duration.bold})`);
+	} catch(err: any) {
+		console.log(`${'✖'.red}  Failed to generate routing index: ${err.message}`);
+
+		throw new Error('Failed to generate routing index', { cause: err });
 	}
 }
